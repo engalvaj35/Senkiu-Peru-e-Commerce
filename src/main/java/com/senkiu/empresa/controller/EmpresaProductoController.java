91db3d85e484caeecff73b616a39d2763a7bbec0
Engelbert Eduardo Alva Jaramillo <engalvaj@gmail.com>
2026-05-27 13:46:12 -0500

    empresa/controller/producto v2

--- src/main/java/com/senkiu/empresa/controller/EmpresaProductoController.java
+++ src/main/java/com/senkiu/empresa/controller/EmpresaProductoController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.MediaType;
 
+import com.senkiu.empresa.repository.EmpresaRepository;
 import com.senkiu.security.CustomUserDetails;
 import com.senkiu.solicitud.producto.dto.SolicitudProductoRequest;
 import com.senkiu.solicitud.producto.service.SolicitudProductoService;
+import com.senkiu.empresa.model.Empresa;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
@@ -17,11 +19,14 @@ import jakarta.validation.Valid;
 public class EmpresaProductoController {
 
     private final SolicitudProductoService solicitudService;
+    private final EmpresaRepository empresaRepository;
 
     public EmpresaProductoController(
-            SolicitudProductoService solicitudService
+            SolicitudProductoService solicitudService,
+            EmpresaRepository empresaRepository
     ) {
         this.solicitudService = solicitudService;
+        this.empresaRepository = empresaRepository;
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -35,8 +40,14 @@ public class EmpresaProductoController {
             @RequestParam("archivo") MultipartFile archivo
     ) {
 
+        Empresa empresa = empresaRepository
+                .findByUsuarioId(user.getId())
+                .orElseThrow(() ->
+                        new RuntimeException("Empresa no encontrada")
+                );
+
         solicitudService.crearSolicitud(
-                user.getId(),
+                empresa.getId(),
                 request,
                 archivo
         );
